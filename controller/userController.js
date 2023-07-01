2e9dd193165cf684d33a55942c151ddc7a6f72d6
satyamgangani <satyamgangani@gmail.com>
2023-07-01 21:37:28 +0530

    update path

--- controller/userController.js
+++ controller/userController.js
@@ -413,7 +413,7 @@ const updateUser = async (req,res)=>{
             let deleteImageURL = await cloudinary.uploader.destroy(user.profilePicPublicId);
           }
           
-          let image = `./temp/${profilePic[0].filename}`;
+          let image = `/temp/${profilePic[0].filename}`;
           let imgUrl = await cloudinary.uploader.upload(image);
           try {
             fs.unlinkSync(`${tempDirPath}${profilePic[0].filename}`);
